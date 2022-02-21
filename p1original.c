f3cec8e7ddec7976c668a463a217cd2ede0db342
Veda R <94687727+VedaRajesh@users.noreply.github.com>
2022-02-21 06:51:54 +0000

    done on 21 feb

--- p1original.c
+++ p1original.c
@@ -1,2 +1,26 @@
+#include <stdio.h>
+#include <math.h>
 
+void input(float *x1, float *y1, float *x2, float *y2)
+{
+    printf("enter 4 numbers\n");
+    scanf("%f%f%f%f",x1,y1,x2,y2);
+}
+void find_distance(float x1, float y1, float x2, float y2, float *area)
+{
+  
+    *area=sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+   
+}
+void output(float x1, float y1,float x2, float y2, float area)
+{
+    printf("distance is %f",area);
+}
+int main()
+{
+    float x1,y1,x2,y2,area;
+    input(&x1,&y1,&x2,&y2);
+    find_distance(x1,y1,x2,y2,&area);
+    output(x1,y1,x2,y2,area);
+    return 0;
 }
