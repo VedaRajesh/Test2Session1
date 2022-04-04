4b20a15532bd14b39dab81e096b3df05c60cb892
Veda R <94687727+VedaRajesh@users.noreply.github.com>
2022-04-04 06:17:22 +0000

    done on 4th april

--- p6original.c
+++ p6original.c
@@ -1,22 +1,46 @@
-#include <stdio.h>
-void input()
+#include<stdio.h>
+void input_string(char *s1,char *s2)
 {
-    char *string;
-    printf("enter a string\n");
-    scanf("%s",string);
+    printf("enter the string and substring\n");
+    scanf("%s%s",s1,s2);
 }
-int string_length(char *string)
+int string_length(char *s2)
 {
-    int i;
-    for(i=0;s[i]!='\0';i++)
-        return 1;
+    int j;
+    for(j=0;s2[j]!='\0';j++);
+    return j;
 }
-int string_cmp(char *string,char *substring)
+int string_index(char *s1,char*s2)
 {
-    for(int i=0;i<n-1 && string[i]==substring[i] &&string[i]!='\0';i++);
-    return string[i]-substring[i];
+
+    int i,j;
+    for(i=0,j=0;s1[i]!='\0' && s2[j]!='\0';i++)
+        {
+            if(s1[i]==s2[j])
+            {
+                j++;
+            }
+            else
+            {
+                j=0;
+            }
+        }
+    return i-j;
 }
-void output(char *string,char *substring)
+void output(char *s1,char *s2,int index,int I)
 {
-    printf("")
+    if(index==I)
+        printf("the index of substring is %d",index);
+    else
+        printf("substring not found");
+}
+int main()
+{
+    char s1[100],s2[100];
+    int index,I;
+    input_string(s1,s2);
+    I= string_length(s2);
+    index=string_index(s1,s2);
+    output(s1,s2,index,I);
+    return 0;
 }
